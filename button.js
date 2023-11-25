652e8a3def7ffa02b68135ef783783628d18768b
AntheaMuscat <74259545+AntheaMuscat@users.noreply.github.com>
2023-11-25 09:28:32 +0100

    Add files via upload

---
+++ button.js
@@ -0,0 +1,82 @@
+function funko1() {
+    var x = document.getElementById("moreInfo");
+    if (x.style.display === "inline") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "inline";
+    }
+}
+
+function funko2() {
+    var x2 = document.getElementById("moreInfo2");
+    if (x2.style.display === "inline") {
+        x2.style.display = "none";
+    } else {
+        x2.style.display = "inline";
+    }
+}
+
+function funko3() {
+    var x3 = document.getElementById("moreInfo3");
+    if (x3.style.display === "inline") {
+        x3.style.display = "none";
+    } else {
+        x3.style.display = "inline";
+    }
+}
+
+function videoGame1() {
+    var x4 = document.getElementById("moreInfo4");
+    if (x4.style.display === "inline") {
+        x4.style.display = "none";
+    } else {
+        x4.style.display = "inline";
+    }
+}
+
+function videoGame2() {
+    var x5 = document.getElementById("moreInfo5");
+    if (x5.style.display === "inline") {
+        x5.style.display = "none";
+    } else {
+        x5.style.display = "inline";
+    }
+}
+
+function videoGame3() {
+    var x6 = document.getElementById("moreInfo6");
+    if (x6.style.display === "inline") {
+        x6.style.display = "none";
+    } else {
+        x6.style.display = "inline";
+    }
+}
+
+function boardGame1() {
+    var x7 = document.getElementById("moreInfo7");
+    if (x7.style.display === "inline") {
+        x7.style.display = "none";
+    } else {
+        x7.style.display = "inline";
+    }
+}
+
+function boardGame2() {
+    var x8 = document.getElementById("moreInfo8");
+    if (x8.style.display === "inline") {
+        x8.style.display = "none";
+    } else {
+        x8.style.display = "inline";
+    }
+}
+
+function boardGame3() {
+    var x9 = document.getElementById("moreInfo9");
+    if (x9.style.display === "inline") {
+        x9.style.display = "none";
+    } else {
+        x9.style.display = "inline";
+    }
+}
+
+
---
+++ menu.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $(".fa").click(function(){
+        $("#menu").toggle("slow");
+    });
+});
